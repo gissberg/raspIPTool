e606ac08e13736a542d38b08cacb0b0cf0909360
gissberg <p.gissberg@gmail.com>
2014-07-08 12:48:56 +0200

    Just tidied up the indentation and comments.

--- raspIPTool.py
+++ raspIPTool.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+# Import needed modules
 from Adafruit_CharLCDPlate import Adafruit_CharLCDPlate
 from subprocess import *
 import socket
@@ -6,9 +7,10 @@ import time
 import os
 from datetime import datetime
 
+# Gather the information
+
 # Initialize the LCD plate.
 # Pass '0' for early 256 MB Model B boards or '1' for all later versions
-
 lcd = Adafruit_CharLCDPlate(busnum = 1)
 lcd.clear()
 cmd = "ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'" #Get IP
@@ -25,28 +27,26 @@ def run_cmd(cmd):
     return output
 
 def is_connected():
-  try:
-    # see if we can resolve the host name -- tells us if there is
-    # a DNS listening
-    host = socket.gethostbyname(REMOTE_SERVER)
-    # connect to the host -- tells us if the host is actually
-    # reachable
-    s = socket.create_connection((host, 80), 2)
-    return True
-  except:
-     pass
-  return False
+    try:
+        # see if we can resolve the host name -- tells us if there is
+        # a DNS listening
+        host = socket.gethostbyname(REMOTE_SERVER)
+        # connect to the host -- tells us if the host is actually
+        # reachable
+        s = socket.create_connection((host, 80), 2)
+        return True
+    except:
+        pass
+        return False
 
 def con_gateway():
-  try:
-    # connect to the gateway -- tells us if the gateway is actually
-    # reachable
-    s = socket.create_connection((gateway, 80), 2)
-    return True
-  except:
-     pass
-  return False
-
+    try:
+        # connect to the gateway -- tells us if the gateway is reachable
+        s = socket.create_connection((gateway, 80), 2)
+        return True
+    except:
+        pass
+        return False
 
 ipaddr = run_cmd(cmd)
 netmask = run_cmd(cmd2)
@@ -56,64 +56,65 @@ getdhcp = run_cmd(cmd5)
 dns = is_connected()
 gwconn = con_gateway()
 
+# Button action below
 pressed_time = None
 
 while True:
     if lcd.buttonPressed(lcd.UP):
-            lcd.backlight(lcd.ON)
-            lcd.clear()
-            lcd.message("Ip-Address:\n")
-            lcd.message(ipaddr )
-            time.sleep(2)
-            lcd.clear()
-            lcd.message("Netmask:\n")
-            lcd.message(netmask )
-            time.sleep(2)
-            lcd.clear()
-            lcd.message("Default GW:\n")
-            lcd.message(gateway )
-            time.sleep(2)
-            lcd.clear()
-            lcd.message(speed )
-            time.sleep(2)
-            lcd.clear()
-            lcd.message("Ip-Address:\n")
-            lcd.message(ipaddr )
+        lcd.backlight(lcd.ON)
+        lcd.clear()
+        lcd.message("Ip-Address:\n")
+        lcd.message(ipaddr )
+        time.sleep(2)
+        lcd.clear()
+        lcd.message("Netmask:\n")
+        lcd.message(netmask )
+        time.sleep(2)
+        lcd.clear()
+        lcd.message("Default GW:\n")
+        lcd.message(gateway )
+        time.sleep(2)
+        lcd.clear()
+        lcd.message(speed )
+        time.sleep(2)
+        lcd.clear()
+        lcd.message("Ip-Address:\n")
+        lcd.message(ipaddr )
 
     elif lcd.buttonPressed(lcd.DOWN):
-            lcd.backlight(lcd.ON)
-            lcd.clear()
-            lcd.message("Connect to GW:\n")
-            lcd.message(gwconn )
-            time.sleep(2)
-            lcd.clear()
-            lcd.message("Resolve google:\n")
-            lcd.message(dns )
+        lcd.backlight(lcd.ON)
+        lcd.clear()
+        lcd.message("Connect to GW:\n")
+        lcd.message(gwconn )
+        time.sleep(2)
+        lcd.clear()
+        lcd.message("Resolve google:\n")
+        lcd.message(dns )
 
     elif lcd.buttonPressed(lcd.LEFT):
-            lcd.backlight(lcd.ON)            
-            lcd.clear()
-            lcd.message("Releasing IP.\n")
-            os.system("sudo dhclient -r")
-            time.sleep(2)
-            lcd.clear()
-            lcd.message("Deleting leases.\n")
-            os.system("sudo rm /var/lib/dhcp/dhclient*")
-            time.sleep(2)
-            lcd.clear()
-            lcd.message("Renewing IP\n")
-            os.system("sudo dhclient eth0")
-            lcd.message(ipaddr )
-            time.sleep(2)
-            lcd.clear()
-            lcd.message("Ip-Address:\n")
-            lcd.message(ipaddr )
+        lcd.backlight(lcd.ON)            
+        lcd.clear()
+        lcd.message("Releasing IP.\n")
+        os.system("sudo dhclient -r")
+        time.sleep(2)
+        lcd.clear()
+        lcd.message("Deleting leases.\n")
+        os.system("sudo rm /var/lib/dhcp/dhclient*")
+        time.sleep(2)
+        lcd.clear()
+        lcd.message("Renewing IP\n")
+        os.system("sudo dhclient eth0")
+        lcd.message(ipaddr )
+        time.sleep(2)
+        lcd.clear()
+        lcd.message("Ip-Address:\n")
+        lcd.message(ipaddr )
 
     elif lcd.buttonPressed(lcd.RIGHT):
-            lcd.backlight(lcd.ON)            
-            lcd.clear()
-            lcd.message("Got IP from:\n")
-            lcd.message(getdhcp )
+        lcd.backlight(lcd.ON)            
+        lcd.clear()
+        lcd.message("Got IP from:\n")
+        lcd.message(getdhcp )
 
     elif lcd.buttonPressed(lcd.SELECT):
         if pressed_time is None:
